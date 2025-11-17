c22d94050fe877e1009f1a6a1169a1f7b5c8e218
Pranav Vikraman s s <pranav.vikraman.25@gmail.com>
2025-11-17 19:00:34 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,17 @@
-import './globals.css'
+import "./globals.css";
 import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ['latin'] })
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Pranav Vikraman — Portfolio',
-  description: 'Electronics & Embedded systems portfolio'
-}
+  title: "Pranav Vikraman — Portfolio",
+  description: "Electronics & Embedded systems portfolio",
+};
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
